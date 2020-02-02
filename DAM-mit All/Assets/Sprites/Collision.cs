bde3a3508526408a80244146d11da0834b7c499c
Jeremy Tsukasa Zimmerman <jtzimmerm@gmail.com>
2020-02-01 23:29:05 -0500

    movement works, still sticks on collision

---
+++ DAM-mit All/Assets/Sprites/Collision.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    BoxCollider BC;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Box Enter");
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        //other.gameObject.broadcast("Repair");
+        BC = other.gameObject.GetComponent<BoxCollider>();
+        BC.isTrigger = false;
+
+    }
+}
